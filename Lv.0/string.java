f88453ee9b5814dbadfdf00626d1f7d85afca7be
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-05 16:06:27 +0900

    scanner 로 입력받은 문자열 출력하기

---
+++ Lv.0/string.java
@@ -0,0 +1,14 @@
+package H;
+import java.util.Scanner;
+
+public class string {
+
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            System.out.println(a);
+        }
+    }
+}
