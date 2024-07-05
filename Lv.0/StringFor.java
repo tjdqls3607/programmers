866b391bcda3e4a0d2dfef615149b66f5d2ac2a2
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-05 17:08:14 +0900

    문자열 반복해서 출력하기

---
+++ Lv.0/StringFor.java
@@ -0,0 +1,16 @@
+package H;
+
+import java.util.Scanner;
+
+public class StringFor {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str = sc.next();
+            int n = sc.nextInt();
+            for(int i=0; i<n; i++) {
+                System.out.printf(str);
+            }
+        }
+    }
+}
