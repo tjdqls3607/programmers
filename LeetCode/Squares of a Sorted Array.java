d4c2c602b68282640f55caf72183bf1066d9c5c6
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-12-12 17:00:26 +0900

    Create Squares of a Sorted Array.java

---
+++ LeetCode/Squares of a Sorted Array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] output = new int[nums.length];
+        for (int i=0; i<nums.length; i++){
+            output[i] = nums[i]*nums[i];
+            
+        }
+        Arrays.sort(output);
+        return output;
+    }
+}
