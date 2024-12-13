803796f881b213a8fa914e8af8003bbc75c0f2c2
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-12-13 16:56:11 +0900

    Create Valid Mountain Array.java

---
+++ LeetCode/Valid Mountain Array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3) return false;
+        int i = 0;
+        
+        while (i+1 < arr.length && arr[i] < arr[i+1] ){
+            i++;
+        }
+        
+        if (i == 0 || i == arr.length - 1){
+            return false;
+        }
+        
+        while (i+1 >arr.length && arr[i] > arr[i+1]){
+            i++;
+        }
+        return i == arr.length - 1;
+    }
+}
